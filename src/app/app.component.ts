1a4fe5adfdef857479896ef17584c69af4a18c3f
catarbusorganizzazione-debug <catarbus.organizzazione@gmail.com>
2025-11-06 20:29:52 +0100

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,7 +5,6 @@ import { RouterModule } from '@angular/router';
   selector: 'app-root',
   standalone: true,
   imports: [RouterModule],
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html'
 })
 export class AppComponent {}
